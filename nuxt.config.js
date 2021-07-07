e0babb3ff6818c0abe38e25ebb1a3f6f66462cad
fxss <723107234@qq.com>
2021-07-07 21:26:07 +0800

    feat: 网站基本配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,33 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog-nuxt',
+    title: process.env.VUE_APP_title || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: process.env.VUE_APP_keywords || '',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.VUE_APP_description || '',
+      },
+      {
+        hid: 'author',
+        name: 'author',
+        content: process.env.VUE_APP_author || '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: `${process.env.VUE_APP_router_base}/favicon.ico`,
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -36,7 +56,31 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: process.env.VUE_APP_host,
+    proxy: true,
+  },
+  proxy: {
+    '/api': {
+      target: process.env.VUE_APP_host,
+      ws: true,
+      changeOrigin: true,
+    },
+  },
+
+  router: {
+    base: process.env.VUE_APP_router_base,
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        return {
+          x: 0,
+          y: 0,
+        }
+      }
+    },
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -48,6 +92,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
   },
   server: {
     port: 8089, // default: 3000
